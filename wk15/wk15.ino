ee61c6a5f62d8a7e172e005e6db7e506354f4c42
gunlinia <39110429+gunlinia@users.noreply.github.com>
2018-06-06 11:16:04 +0800

    Add files via upload

---
+++ wk15/wk15.ino
@@ -0,0 +1,22 @@
+
+const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
+const int analogOutPin = 9; // Analog output pin that the LED is attached to
+
+int sensorValue = 0;        // value read from the pot
+int outputValue = 0;        // value output to the PWM (analog out)
+
+void setup() {
+  // initialize serial communications at 9600 bps:
+  Serial.begin(9600);}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+int sensorRead =analogRead(A0);
+if (sensorRead<600)sensorRead=600;
+else if(sensorRead>900)sensorRead=900;
+int outputValue=map(sensorRead,600,900,0,255);
+analogWrite(9,outputValue);
+Serial.println(outputValue);
+delay(200);
+}
